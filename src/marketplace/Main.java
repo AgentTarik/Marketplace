fed65b08224f63b81046ab3613eed66990c5291c
AgentTarik <agenttarik99672@gmail.com>
2023-09-01 09:00:07 -0300

    new changes
    new changes

--- src/marketplace/Main.java
+++ src/marketplace/Main.java
@@ -1,4 +1,4 @@
-package donabenta;
+package marketplace;
 
 import controller.UserController;
 import view.LoginScreen;
@@ -7,15 +7,17 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
     public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        UserController userController = new UserController();
+        userController.read();
         launch(args);
     }
 
-
     @Override
     public void start(Stage primaryStage) throws Exception {
-
-        primaryStage.setHeight(600);
-        primaryStage.setWidth(600);
+        primaryStage.setHeight(400);
+        primaryStage.setWidth(400);
         LoginScreen loginScreen = new LoginScreen(primaryStage);
         primaryStage.setScene(loginScreen.getScene());
         primaryStage.show();
