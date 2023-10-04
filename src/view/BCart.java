0a2a8275fec39cd8867ffe7b3422d2cc8fca0f17
AgentTarik <agenttarik99672@gmail.com>
2023-10-04 15:03:11 -0300

    cart

--- src/view/BCart.java
+++ src/view/BCart.java
@@ -1,21 +1,29 @@
 package view;
 
+import controller.PartsController;
+import controller.PurchaseController;
+import javafx.collections.FXCollections;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+import model.Computer;
 
 public class BCart {
     private Scene scene;
-    private Button back;
+    private Button back,buyButton;
     private BLobby bLobby;
-    private Label title;
+    private Alert successAlert,failAlert;
+    private TableView table;
+    private PurchaseController purchaseController;
+    private Label title,cartValue;
     private VBox mainBox;
-    private HBox backBox;
+    private HBox backBox,buyBox;
 
 
     public BCart(String activeUser, Stage primaryStage){
@@ -36,10 +44,56 @@ public class BCart {
             primaryStage.setScene(bLobby.getScene());
         });
 
+        purchaseController = new PurchaseController();
+
+        table = new TableView();
+        table.setItems(FXCollections.observableList(purchaseController.read(activeUser)));
+
+        TableColumn name = new TableColumn<>("ComputerName");
+        name.setPrefWidth(100);
+        name.setCellValueFactory(new PropertyValueFactory<>("ComputerName"));
+
+        TableColumn quantity = new TableColumn<>("PartName");
+        quantity.setPrefWidth(100);
+        quantity.setCellValueFactory(new PropertyValueFactory<>("PartName"));
+
+        TableColumn price = new TableColumn<>("Value");
+        price.setPrefWidth(100);
+        price.setCellValueFactory(new PropertyValueFactory<>("Value"));
+
+        table.getColumns().setAll(name,quantity,price);
+
+        cartValue = new Label();
+        cartValue.setText("The total value of your cart is: $"+ purchaseController.cartValue(activeUser));
+        cartValue.setFont(Font.font("Verdana", FontWeight.BOLD, 12));
+
+        buyButton = new Button();
+        buyButton.setText("Buy Now");
+        buyButton.setOnAction(event -> {
+            if (purchaseController.read(activeUser)!=null){
+                purchaseController.delete(activeUser);
+                successAlert = new Alert(Alert.AlertType.INFORMATION);
+                successAlert.setTitle("Success");
+                successAlert.setHeaderText(null);
+                successAlert.setContentText("Thank you for purchasing!");
+                successAlert.showAndWait();
+            }else{
+                failAlert = new Alert(Alert.AlertType.INFORMATION);
+                failAlert.setTitle(":c");
+                failAlert.setHeaderText(null);
+                failAlert.setContentText("Your cart is empty");
+                failAlert.showAndWait();
+            }
+        });
+
+
         mainBox = new VBox();
+        buyBox = new HBox();
+        buyBox.getChildren().addAll(cartValue,buyButton);
+        buyBox.setAlignment(Pos.CENTER);
         backBox = new HBox();
         backBox.getChildren().addAll(back);
-        mainBox.getChildren().addAll(backBox,title);
+        mainBox.getChildren().addAll(backBox,title,table,buyBox);
         mainBox.setAlignment(Pos.CENTER);
         scene = new Scene(mainBox);
     }
